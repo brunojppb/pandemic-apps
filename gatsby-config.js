1916bfb164472b4964c67b9f942a05beacf6394a
Bruno Paulino <bruno@woombikes.com>
2020-03-16 17:15:51 +0100

    update analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "UA-66025495-5",
       },
     },
     {
